bd65d0130d6e095e7dbf02b00773f47af807a051
assyfanaziwa <83399060+assyfanaziwa@users.noreply.github.com>
2021-07-08 14:11:16 +0700

    Update Infix-Postfix-1.cpp

--- Infix-Postfix-1.cpp
+++ Infix-Postfix-1.cpp
@@ -2,6 +2,17 @@
 #include <string>
 using namespace std;
 
+int main(){
+string infix2,simpan[100],infix;
+int jumlah = 0,panjang;
+getline(cin,infix2);
+
+panjang = infix2.length();
+for(int i = 0; i<panjang;i++){
+    if (infix2[i] != ' '){
+        infix += infix2[i];
+    }
+}
 
 bool operand(char a){
 int asci = (int)a;
