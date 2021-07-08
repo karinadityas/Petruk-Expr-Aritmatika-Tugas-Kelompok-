37cf772fc5e10ccba72f4459e950e4af91ec308d
akbarananto12 <83356837+akbarananto12@users.noreply.github.com>
2021-07-08 14:08:58 +0700

    Create Infix-Postfix-1.cpp

---
+++ Infix-Postfix-1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+bool operand(char a){
+int asci = (int)a;
+if (asci >= 48 && asci <= 57){
+    return true;
+} else {return false;}
+
+}
+
+bool is_operator(char a){
+int asci = (int)a;
+if (!(asci >= 48 && asci <= 57)){
+    return true;
+} else {return false;}
+
+}
